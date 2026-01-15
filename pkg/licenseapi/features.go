4ac6f1d3ad09e78403ad0f33c9eb6c102732d55a
Ricardo Weir <ricardo.weir@loft.sh>
2026-01-15 11:31:34 -0700

    Generate modules
    Prior, modules had to be manually entered in addition to making
    changes to features.yaml. Now, generation of feature and module
    files are driven from the features.yaml and modules.yaml files.

--- pkg/licenseapi/features.go
+++ pkg/licenseapi/features.go
@@ -196,242 +196,302 @@ func GetAllFeatures() []*Feature {
  		{
 			DisplayName: "Virtual Cluster Management",
 			Name:        "vclusters",
+			Module:      "virtual-clusters",
 		},
 		{
 			DisplayName: "Sleep Mode for Virtual Clusters",
 			Name:        "vcluster-sleep-mode",
+			Module:      "virtual-clusters",
 		},
 		{
 			DisplayName: "Central HostPath Mapper",
 			Name:        "vcluster-host-path-mapper",
+			Module:      "virtual-clusters",
 		},
 		{
 			DisplayName: "Enterprise Plugins",
 			Name:        "vcluster-enterprise-plugins",
+			Module:      "virtual-clusters",
 		},
 		{
 			DisplayName: "Security-Hardened vCluster Image",
 			Name:        "vcp-distro-image",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Built-In CoreDNS",
 			Name:        "vcp-distro-built-in-coredns",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Virtual Admission Control",
 			Name:        "vcp-distro-admission-control",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Sync Patches",
 			Name:        "vcp-distro-sync-patches",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Embedded etcd",
 			Name:        "vcp-distro-embedded-etcd",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Isolated Control Plane",
 			Name:        "vcp-distro-isolated-cp",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Centralized Admission Control",
 			Name:        "vcp-distro-centralized-admission-control",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Generic Sync",
 			Name:        "vcp-distro-generic-sync",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Translate Patches",
 			Name:        "vcp-distro-translate-patches",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "KubeVirt Integration",
 			Name:        "vcp-distro-integrations-kube-virt",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "External Secrets Integration",
 			Name:        "vcp-distro-integrations-external-secrets",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Cert Manager Integration",
 			Name:        "vcp-distro-integrations-cert-manager",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "FIPS",
 			Name:        "vcp-distro-fips",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "External Database",
 			Name:        "vcp-distro-external-database",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Private Nodes",
 			Name:        "vcp-distro-private-nodes",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "External Database RDS IAM Authentication",
 			Name:        "external-database-rds-iam",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Database Connector",
 			Name:        "connector-external-database",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "EKS Pod Identity for External Database Connections",
 			Name:        "connector-external-database-eks-pod-identity",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "SleepMode",
 			Name:        "vcp-distro-sleep-mode",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Dev Environment Management",
 			Name:        "devpod",
+			Module:      "dev-environments",
 		},
 		{
 			DisplayName: "Namespace Management",
 			Name:        "namespaces",
+			Module:      "kubernetes-namespaces",
 		},
 		{
 			DisplayName: "Sleep Mode for Namespaces",
 			Name:        "namespace-sleep-mode",
+			Module:      "kubernetes-namespaces",
 		},
 		{
 			DisplayName: "Connected Clusters",
 			Name:        "connected-clusters",
+			Module:      "kubernetes-clusters",
 		},
 		{
 			DisplayName: "Cluster Access",
 			Name:        "cluster-access",
+			Module:      "kubernetes-clusters",
 		},
 		{
 			DisplayName: "Cluster Role Management",
 			Name:        "cluster-roles",
+			Module:      "kubernetes-clusters",
 		},
 		{
 			DisplayName: "Single Sign-On",
 			Name:        "sso-authentication",
+			Module:      "auth-audit-logging",
 		},
 		{
 			DisplayName: "Audit Logging",
 			Name:        "audit-logging",
+			Module:      "auth-audit-logging",
 		},
 		{
 			DisplayName: "Automatic Auth For Ingresses",
 			Name:        "auto-ingress-authentication",
+			Module:      "auth-audit-logging",
 		},
 		{
 			DisplayName: "Platform as OIDC Provider",
 			Name:        "oidc-provider",
+			Module:      "auth-audit-logging",
 		},
 		{
 			DisplayName: "Multiple SSO Providers",
 			Name:        "multiple-sso-providers",
+			Module:      "auth-audit-logging",
 		},
 		{
 			DisplayName: "Apps",
 			Name:        "apps",
+			Module:      "templating-gitops",
 		},
 		{
 			DisplayName: "Template Versioning",
 			Name:        "template-versioning",
+			Module:      "templating-gitops",
 		},
 		{
 			DisplayName: "Argo Integration",
 			Name:        "argo-integration",
+			Module:      "templating-gitops",
 		},
 		{
 			DisplayName: "Rancher Integration",
 			Name:        "rancher-integration",
+			Module:      "templating-gitops",
 		},
 		{
 			DisplayName: "Secrets Sync",
 			Name:        "secrets",
+			Module:      "secrets-management",
 		},
 		{
 			DisplayName: "Secrets Encryption",
 			Name:        "secret-encryption",
+			Module:      "secrets-management",
 		},
 		{
 			DisplayName: "HashiCorp Vault Integration",
 			Name:        "vault-integration",
+			Module:      "secrets-management",
 		},
 		{
 			DisplayName: "High-Availability Mode",
 			Name:        "ha-mode",
+			Module:      "deployment-modes",
 		},
 		{
 			DisplayName: "Multi-Region Mode",
 			Name:        "multi-region-mode",
+			Module:      "deployment-modes",
 		},
 		{
 			DisplayName: "Air-Gapped Mode",
 			Name:        "air-gapped-mode",
+			Module:      "deployment-modes",
 		},
 		{
 			DisplayName: "Custom Branding",
 			Name:        "custom-branding",
+			Module:      "ui-customization",
 		},
 		{
 			DisplayName: "Advanced UI Customizations",
 			Name:        "advanced-ui-customizations",
+			Module:      "ui-customization",
 		},
 		{
 			DisplayName: "vNode Runtime",
 			Name:        "vnode-runtime",
+			Module:      "vnode",
 		},
 		{
 			DisplayName: "Project Quotas",
 			Name:        "project-quotas",
+			Module:      "virtual-clusters",
 		},
 		{
 			DisplayName: "Resolve DNS",
 			Name:        "resolve-dns",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Istio Integration",
 			Name:        "istio-integration",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Hybrid Scheduling",
 			Name:        "hybrid-scheduling",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Sync Namespaces toHost",
 			Name:        "sync-namespaces-tohost",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Auto Snapshots",
 			Name:        "scheduled-snapshots",
+			Module:      "virtual-clusters",
 		},
 		{
 			DisplayName: "Private Nodes VPN",
 			Name:        "private-nodes-vpn",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Private Nodes Auto Nodes",
 			Name:        "private-nodes-auto-nodes",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Disable Platform Database",
 			Name:        "disable-platform-db",
+			Module:      "deployment-modes",
 		},
 		{
 			DisplayName: "Standalone",
 			Name:        "standalone",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Netris",
 			Name:        "netris",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "Kube-vip Integration",
 			Name:        "kube-vip",
+			Module:      "vcluster-pro-distro",
 		},
 		{
 			DisplayName: "vCluster Proxy Resources",
 			Name:        "vcluster-proxy-resources",
+			Module:      "vcluster-pro-distro",
 		},
  	}
 }
