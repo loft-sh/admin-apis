3c4cb7905cfa68c55031b6b12c88968f6f588fcd
Caleb Bron <cbron@users.noreply.github.com>
2026-07-01 13:07:53 -0600

    Adds observability (#102)

--- pkg/licenseapi/features.go
+++ pkg/licenseapi/features.go
@@ -10,6 +10,8 @@ const (
 
 	ConnectedClusters FeatureName = "connected-clusters" // Connected Clusters
 
+	Observability FeatureName = "observability" // Observability
+
 	Secrets FeatureName = "secrets" // Secrets Sync
 
 	Apps FeatureName = "apps" // Apps
@@ -161,6 +163,7 @@ func GetFeatures() []FeatureName {
 		ClusterAccess,
 		ClusterRoles,
 		ConnectedClusters,
+		Observability,
 		Secrets,
 		Apps,
 		Namespaces,
@@ -251,6 +254,11 @@ func GetAllFeatures() []*Feature {
 		{
 			DisplayName: "Connected Clusters",
 			Name:        "connected-clusters",
+			Module:      "platform",
+		},
+		{
+			DisplayName: "Observability",
+			Name:        "observability",
 			Module:      "platform",
 		},
 		{
